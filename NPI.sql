0ea460e28a62b9c9e3b1bc0515eac7224bacbfa7
Astrid Willis Countee <astridwcountee@gmail.com>
2015-04-22 16:08:59 -0500

    started new query to limit result set, need unique table of npis

--- NPI.sql
+++ NPI.sql
@@ -1,214 +1,226 @@
 
--- Query to show the records that are shared by both years for on Npi
-select *
-from npi_team_2012_2013_365.undirected a
-inner join npi_team_2013_2014_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi= 1720027436
+-- Query to show the records that are shared by both years for ON Npi
+SELECT *
+FROM npi_team_2012_2013_365.undirected a
+INNER JOIN npi_team_2013_2014_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi= 1720027436
 --151 records
 
 
---Query to show new relationships that did not exist in the previous year
-select *
-from npi_team_2013_2014_365.undirected a
-left join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where b.npi is null and b.npi_dest is null and a.npi= 1720027436
+--Query to show new relatiONships that did not exist in the previous year
+SELECT *
+FROM npi_team_2013_2014_365.undirected a
+LEFT JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436
 --34 records
 
 
 --same as above without alias for tables
-select *
-from npi_team_2013_2014_365.undirected
-left join npi_team_2012_2013_365.undirected
-on npi_team_2013_2014_365.undirected.npi = npi_team_2012_2013_365.undirected.npi
-and npi_team_2013_2014_365.undirected.npi_dest = npi_team_2012_2013_365.undirected.npi_dest
-where npi_team_2012_2013_365.undirected.npi is null and npi_team_2012_2013_365.undirected.npi_dest is null
-and a.npi= 1720027436
+SELECT *
+FROM npi_team_2013_2014_365.undirected
+LEFT JOIN npi_team_2012_2013_365.undirected
+ON npi_team_2013_2014_365.undirected.npi = npi_team_2012_2013_365.undirected.npi
+AND npi_team_2013_2014_365.undirected.npi_dest = npi_team_2012_2013_365.undirected.npi_dest
+WHERE npi_team_2012_2013_365.undirected.npi is NULL AND npi_team_2012_2013_365.undirected.npi_dest is NULL
+AND a.npi= 1720027436
 
 
---Query to count current relationships
-select distinct count(npi_dest) as 'unique count of relationships'
-from npi_team_2013_2014_365.undirected
-where npi= 1720027436
+--Query to count current relatiONships
+SELECT DISTINCT count(npi_dest) as 'unique count of relatiONships'
+FROM npi_team_2013_2014_365.undirected
+WHERE npi= 1720027436
 
 --sum of new npi_dest patient count
-select sum(patient_count)
-from npi_team_2013_2014_365.undirected
-where npi=1720027436 and npi_dest in (
-  select a.npi_dest
-  from npi_team_2013_2014_365.undirected a
-  left join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null and a.npi= 1720027436)
+SELECT sum(patient_count)
+FROM npi_team_2013_2014_365.undirected
+WHERE npi=1720027436 AND npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2013_2014_365.undirected a
+  LEFT JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436)
 
 
 
---count of lost relationships and lost patients
-select distinct count(npi_dest) as 'lost npi relationship count', sum(patient_count) as 'lost patient count'
-from npi_team_2012_2013_365.undirected
-where npi=1720027436 and npi_dest in (
-  select a.npi_dest
-  from npi_team_2012_2013_365.undirected a
-  left join npi_team_2013_2014_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null and a.npi= 1720027436)
+--count of lost relatiONships AND lost patients
+SELECT DISTINCT count(npi_dest) as 'lost npi relatiONship count', sum(patient_count) as 'lost patient count'
+FROM npi_team_2012_2013_365.undirected
+WHERE npi=1720027436 AND npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2012_2013_365.undirected a
+  LEFT JOIN npi_team_2013_2014_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436)
 
---count of new relationships and new patients
-select distinct count(npi_dest) as 'new npi relationship count', sum(patient_count) as 'new patient count'
-from npi_team_2013_2014_365.undirected
-where npi=1720027436 and npi_dest in (
-  select a.npi_dest
-  from npi_team_2013_2014_365.undirected a
-  left join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null and a.npi= 1720027436)
+--count of new relatiONships AND new patients
+SELECT DISTINCT count(npi_dest) as 'new npi relatiONship count', sum(patient_count) as 'new patient count'
+FROM npi_team_2013_2014_365.undirected
+WHERE npi=1720027436 AND npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2013_2014_365.undirected a
+  LEFT JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436)
 
 
---Patient flow change from previous year's data
-select (sum(a.patient_count) - sum(b.patient_count)) as 'patient flow change'
-from npi_team_2013_2014_365.undirected a
-inner join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi = 1720027436
+--Patient flow change FROM previous year's data
+SELECT (sum(a.patient_count) - sum(b.patient_count)) as 'patient flow change'
+FROM npi_team_2013_2014_365.undirected a
+INNER JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi = 1720027436
 
 --patient flow change average
-select asum / bcount
-from
-(select abs(sum(a.patient_count - b.patient_count)) asum
-from npi_team_2013_2014_365.undirected a
-inner join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi = 1720027436) a, (select count(a.npi_dest) bcount
-from npi_team_2012_2013_365.undirected a
-inner join npi_team_2013_2014_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi= 1720027436) b
+SELECT asum / bcount
+FROM
+(SELECT abs(sum(a.patient_count - b.patient_count)) asum
+FROM npi_team_2013_2014_365.undirected a
+INNER JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi = 1720027436) a, (SELECT count(a.npi_dest) bcount
+FROM npi_team_2012_2013_365.undirected a
+INNER JOIN npi_team_2013_2014_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi= 1720027436) b
 
 --patient flow change percentage
-select (asum / bsum)/ ccount
-from (select (sum(a.patient_count) - sum(b.patient_count)) asum
-from npi_team_2013_2014_365.undirected a
-inner join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi = 1720027436) a,
-( select sum(b.patient_count) bsum
-from npi_team_2012_2013_365.undirected b
-where npi = 1720027436) b,
-(select count(a.npi_dest) ccount
-from npi_team_2012_2013_365.undirected a
-inner join npi_team_2013_2014_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi= 1720027436) c
+SELECT (asum / bsum)/ ccount
+FROM (SELECT (sum(a.patient_count) - sum(b.patient_count)) asum
+FROM npi_team_2013_2014_365.undirected a
+INNER JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi = 1720027436) a,
+( SELECT sum(b.patient_count) bsum
+FROM npi_team_2012_2013_365.undirected b
+WHERE npi = 1720027436) b,
+(SELECT count(a.npi_dest) ccount
+FROM npi_team_2012_2013_365.undirected a
+INNER JOIN npi_team_2013_2014_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi= 1720027436) c
 
 --absolute patient flow change query test(2 queries)
--- select abs(sum(a.patient_count - b.patient_count)) as 'difference'
--- from npi_team_2013_2014_365.undirected a
--- inner join npi_team_2012_2013_365.undirected b
--- on a.npi = b.npi and a.npi_dest = b.npi_dest
--- where a.npi = 1720027436 and a.patient_count - b.patient_count < 0;
--- select abs(sum(a.patient_count - b.patient_count)) as 'difference'
--- from npi_team_2013_2014_365.undirected a
--- inner join npi_team_2012_2013_365.undirected b
--- on a.npi = b.npi and a.npi_dest = b.npi_dest
--- where a.npi = 1720027436 and a.patient_count - b.patient_count > 0
+-- SELECT abs(sum(a.patient_count - b.patient_count)) as 'difference'
+-- FROM npi_team_2013_2014_365.undirected a
+-- INNER JOIN npi_team_2012_2013_365.undirected b
+-- ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+-- WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count < 0;
+-- SELECT abs(sum(a.patient_count - b.patient_count)) as 'difference'
+-- FROM npi_team_2013_2014_365.undirected a
+-- INNER JOIN npi_team_2012_2013_365.undirected b
+-- ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+-- WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count > 0
 
 --combined query to get total absolute value
-select a.asum + b.bsum
-from
-(select abs(sum(a.patient_count - b.patient_count)) asum
-from npi_team_2013_2014_365.undirected a
-inner join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi = 1720027436 and a.patient_count - b.patient_count < 0) as a,
-(select abs(sum(a.patient_count - b.patient_count)) bsum
-from npi_team_2013_2014_365.undirected a
-inner join npi_team_2012_2013_365.undirected b
-on a.npi = b.npi and a.npi_dest = b.npi_dest
-where a.npi = 1720027436 and a.patient_count - b.patient_count > 0) as b
+SELECT a.asum + b.bsum
+FROM
+(SELECT abs(sum(a.patient_count - b.patient_count)) asum
+FROM npi_team_2013_2014_365.undirected a
+INNER JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count < 0) as a,
+(SELECT abs(sum(a.patient_count - b.patient_count)) bsum
+FROM npi_team_2013_2014_365.undirected a
+INNER JOIN npi_team_2012_2013_365.undirected b
+ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count > 0) as b
 
 
 ---score
 
-select (a.asum * 5) + (b.bsum * 5) + c.csum
-from
-(select sum(patient_count) asum
-from npi_team_2012_2013_365.undirected
-where npi=1720027436 and npi_dest in (
-  select a.npi_dest
-  from npi_team_2012_2013_365.undirected a
-  left join npi_team_2013_2014_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null and a.npi= 1720027436)) a,
-(select sum(patient_count) bsum
-from npi_team_2013_2014_365.undirected
-where npi=1720027436 and npi_dest in (
-  select a.npi_dest
-  from npi_team_2013_2014_365.undirected a
-  left join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null and a.npi= 1720027436)) b,
-  (select (x.asum + y.bsum) csum
-  from
-  (select abs(sum(a.patient_count - b.patient_count)) asum
-  from npi_team_2013_2014_365.undirected a
-  inner join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where a.npi = 1720027436 and a.patient_count - b.patient_count < 0) as x,
-  (select abs(sum(a.patient_count - b.patient_count)) bsum
-  from npi_team_2013_2014_365.undirected a
-  inner join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where a.npi = 1720027436 and a.patient_count - b.patient_count > 0) as y) c
+SELECT (a.asum * 5) + (b.bsum * 5) + c.csum
+FROM
+(SELECT sum(patient_count) asum
+FROM npi_team_2012_2013_365.undirected
+WHERE npi=1720027436 AND npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2012_2013_365.undirected a
+  LEFT JOIN npi_team_2013_2014_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436)) a,
+(SELECT sum(patient_count) bsum
+FROM npi_team_2013_2014_365.undirected
+WHERE npi=1720027436 AND npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2013_2014_365.undirected a
+  LEFT JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL AND a.npi= 1720027436)) b,
+  (SELECT (x.asum + y.bsum) csum
+  FROM
+  (SELECT abs(sum(a.patient_count - b.patient_count)) asum
+  FROM npi_team_2013_2014_365.undirected a
+  INNER JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count < 0) as x,
+  (SELECT abs(sum(a.patient_count - b.patient_count)) bsum
+  FROM npi_team_2013_2014_365.undirected a
+  INNER JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE a.npi = 1720027436 AND a.patient_count - b.patient_count > 0) as y) c
 
 
--- alma's work with fred. 
--- rather than doing the calculation we are returning the foundational data points along with associated npi
+-- alma's work with fred.
+-- rather than doing the calculatiON we are returning the foundatiONal data points alONg with associated npi
 -- we removed the WHERE clauses that were restricting the query to a single npi.
--- the resulting query may be different in nature because the npi limitation may have been doing more than just restricting the results by npi
+-- the resulting query may be different in nature because the npi limitatiON may have been doing more than just restricting the results by npi
 
--- TO DO: 
--- since this is a query based on sub-queries, we should use LIMIT to ensure that every sub-query is only returning 1000 npis for testing purposes
--- using LIMIT, you should be able to ensure that the code would work on all npis, without tying up the server for hours or days
--- then use EXPLAIN in order to verify that wherever possible, your query is leveraging indexes. Add indexes as needed.
--- use the CREATE TABLE syntax with LIMIT removed, in order create a new data table once you have proved to yourself that the query is correct
+-- TO DO:
+-- since this is a query based ON sub-queries, we should use LIMIT to ensure that every sub-query is ONly returning 1000 npis for testing purposes
+-- using LIMIT, you should be able to ensure that the code would work ON all npis, without tying up the server for hours or days
+-- then use EXPLAIN in order to verify that WHEREver possible, your query is leveraging indexes. Add indexes as needed.
+-- use the CREATE TABLE syntax with LIMIT removed, in order create a new data table ONce you have proved to yourself that the query is correct
 -- then run the query, hours or days taken up is fine.
--- you need a way to make sure sub-queries are working on the same npi at the same time, now that the WHERE has been removed
+-- you need a way to make sure sub-queries are working ON the same npi at the same time, now that the WHERE has been removed
 
 -- Fixing this mega query is not necesarily the easiest way (can also use intermediate tables), but having a properly named four-column table with the scores for each npi is the goal
 
 
 SELECT
-	npi, 
-	a.asum, 
+	npi,
+	a.asum,
 	b.bsum,
 	c.csum
-from
-(select npi, sum(patient_count) asum
-from npi_team_2012_2013_365.undirected
-where npi_dest in (
-  select a.npi_dest
-  from npi_team_2012_2013_365.undirected a
-  left join npi_team_2013_2014_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null)) a,
-(select sum(patient_count) bsum
-from npi_team_2013_2014_365.undirected
-where npi_dest in (
-  select a.npi_dest
-  from npi_team_2013_2014_365.undirected a
-  left join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where b.npi is null and b.npi_dest is null)) b,
-  (select (x.asum + y.bsum) csum
-  from
-  (select abs(sum(a.patient_count - b.patient_count)) asum
-  from npi_team_2013_2014_365.undirected a
-  inner join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where a.patient_count - b.patient_count < 0) as x,
-  (select abs(sum(a.patient_count - b.patient_count)) bsum
-  from npi_team_2013_2014_365.undirected a
-  inner join npi_team_2012_2013_365.undirected b
-  on a.npi = b.npi and a.npi_dest = b.npi_dest
-  where a.patient_count - b.patient_count > 0) as y) c
+FROM
+(SELECT npi, sum(patient_count) asum
+FROM npi_team_2012_2013_365.undirected
+WHERE npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2012_2013_365.undirected a
+  LEFT JOIN npi_team_2013_2014_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL)) a ,
+(SELECT sum(patient_count) bsum
+FROM npi_team_2013_2014_365.undirected
+WHERE npi_dest in (
+  SELECT a.npi_dest
+  FROM npi_team_2013_2014_365.undirected a
+  LEFT JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE b.npi is NULL AND b.npi_dest is NULL)) b,
+  (SELECT (x.asum + y.bsum) csum
+  FROM
+  (SELECT abs(sum(a.patient_count - b.patient_count)) asum
+  FROM npi_team_2013_2014_365.undirected a
+  INNER JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE a.patient_count - b.patient_count < 0) as x,
+  (SELECT abs(sum(a.patient_count - b.patient_count)) bsum
+  FROM npi_team_2013_2014_365.undirected a
+  INNER JOIN npi_team_2012_2013_365.undirected b
+  ON a.npi = b.npi AND a.npi_dest = b.npi_dest
+  WHERE a.patient_count - b.patient_count > 0) as y) c
+
+
+
+
+--attempt to join the npi databases with npi table to other tables to have a unique set
+--still working on it
+--If there is a way for me to connect to this table or to have it available inside the team foler
+--it would take care of the problem of having a table that has unique NPIs
+  SELECT n.npi, a.npi
+  FROM npi n LIMIT 10
+  INNER JOIN npi_team_2012_2013_365.undirected a
+  ON npi.npi = a.npi
---
+++ README.md
@@ -0,0 +1,4 @@
+# docgraph_journalists
+Private repo for the work done by docgraph journalists
+
+This is a private work space to record the work done on various data projects
